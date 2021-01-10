86165422c2df31bee1f68469b98d657014ceefec
Yunseon Choi <science_co@naver.com>
2021-01-11 02:21:41 +0900

    CounterContainer 구현 완료

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from "react";
 
 import "./App.css";
-import Counter from "./components/Counter";
 import WaitingList from "./components/WaitingList";
-import PaletteContainer from "./containers/PaletteContainer"; // **** (1) 불러오기
-
+import PaletteContainer from "./containers/PaletteContainer";
+import CounterContainer from "./containers/CounterContainer"; // **** (1) 불러오기
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <PaletteContainer /> {/* **** (2) 대체하기 */}
-        <Counter value={0} color="red" />
+        <PaletteContainer />
+        <CounterContainer /> {/* ****(2) 대체하기 */}
         <WaitingList />
       </div>
     );
