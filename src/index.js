fe0a510ff065d661b2b6d75521d119846fed6b24
Yunseon Choi <science_co@naver.com>
2021-01-11 01:47:15 +0900

    Redux Devtools Use Redux Devtools when creating store

--- src/index.js
+++ src/index.js
@@ -8,9 +8,10 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
-// **** (2) 스토어를 만들고 현재 값 확인해보기
-const store = createStore(rootReducer);
-console.log(store.getState());
+// **** 리덕스 개발자도구 적용
+const devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(rootReducer, devTools);
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
